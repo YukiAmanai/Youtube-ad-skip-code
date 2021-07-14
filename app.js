b397cc552471220c95177b41ca1c0d0b856e87a7
YukiAmanai <yuki_amanai@icloud.com>
2021-07-14 16:07:06 +0900

    try catchの修正

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ setInterval(
         try {
             const skipButton = document.getElementsByClassName('ytp-ad-skip-button-container');
             skipButton[0].click();
-        } catch (error) {
-            console.log(error);
+        } catch (e) {
+            ;
         }
     }
 ), 1000
