655536e619851b789d05ee7acf4ab0463bd8b6cf
YukiAmanai <yuki_amanai@icloud.com>
2021-09-05 20:47:06 +0900

    アロー関数に修正

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-setInterval(
-    function () {
-        try {
-            const adButtonClass = document.getElementsByClassName('ytp-ad-skip-button-container');
-            const adButtonElem = adButtonClass[0];
-            adButtonElem.click();
-        } catch (e) {
-            ;
-        }
+const adSkipButton = () => {
+    try {
+        const adButtonClass = document.getElementsByClassName('ytp-ad-skip-button-container');
+        const adButtonElem = adButtonClass[0];
+        adButtonElem.click();
+    } catch (e) {
+        ;
     }
-), 1000
-
+}
+setInterval(adSkipButton ,1000);
